1af6bca1ffdbf07101fdea6421283e99f4c37cec
ganesh-redy <ganeshreddy7610@gmail.com>
2025-04-23 15:51:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+    project = "sam-453310"
+    zone = "us-central1-a"
+
+  
+}
+resource "google_compute_instance" "instance1" {
+    name = "instance"
+    machine_type = "e2-micro"
+    boot_disk {
+        initialize_params {
+          image = "centos-stream-9"
+          size = 30
+        }
+    
+    }   
+    network_interface {
+        network = "default"
+      access_config {
+        
+      }
+    }
+}

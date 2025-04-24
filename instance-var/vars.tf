809416a0b983a7e47cd520d357f8cd2ba6f1ac01
ganesh-redy <ganeshreddy7610@gmail.com>
2025-04-24 10:29:43 +0530

    Create vars.tf

---
+++ instance-var/vars.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    type = "string"
+  
+}
+variable "zone" {
+    type = "string"
+    default = "us-central1-a"
+  
+}
+variable "name" {
+    type = "string"
+  
+}
+variable "image" {
+    type = "string"
+  
+}
